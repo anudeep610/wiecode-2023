57ac58f6c6cba7e952fb96cb376315a484b84ba2
Anudeep <anudeep.hanuman@gmail.com>
2023-01-06 04:31:42 +0530

    added contact us page

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -7,7 +7,7 @@ import "../css/Registration.css";
 import Part1 from "../components/forms/Part-1";
 import Part2 from "../components/forms/Part-2";
 import Part3 from "../components/forms/Part-3";
-
+import Loader from "../components/loader";
 import Footer from './Footer';
 
 export default function Registration() {
@@ -16,32 +16,32 @@ export default function Registration() {
     const user = useContext(userContext);
     const [page, setPage] = useState(0);
     const [loading, setLoading] = useState(false)
-    const titles = ["Wie Code 2023", "Team Details", "Domain & Abstract"];
+    const titles = ["Hola!😄 ", "Team Details❓", "Domain & Abstract🤔"];
     const [teamSize, setTeamSize] = useState(0);
     const [teamName, setTeamName] = useState("");
     const [domain, setDomain] = useState("");
     const [teamDetails, setTeamDetails] = useState([]);
     const [abstract, setAbstract] = useState();
 
-    const handleSubmit = async() => {
+    const handleSubmit = async () => {
         setLoading(true);
         const form = new FormData();
-        form.append('abstract',abstract);
-        form.append('teamSize',teamSize);
-        form.append('domain',domain);
-        form.append('teamName',teamName);
+        form.append('abstract', abstract);
+        form.append('teamSize', teamSize);
+        form.append('domain', domain);
+        form.append('teamName', teamName);
         form.append('teamDetails', JSON.stringify(teamDetails));
-        console.log(teamDetails,teamSize,teamName,domain,abstract);
-        const res = await axios.post("/register",form,{
-            headers:{
-                "Content-Type":"multipart/form-data"
+        console.log(teamDetails, teamSize, teamName, domain, abstract);
+        const res = await axios.post("/register", form, {
+            headers: {
+                "Content-Type": "multipart/form-data"
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
             alert("something went wrong ;) please try again later");
             navigate("/");
         });
-        if(res.data.status===200){
+        if (res.data.status === 200) {
             alert("We have received your abstract. All the best :)");
             navigate("/");
         }
@@ -50,57 +50,57 @@ export default function Registration() {
 
     const PageDisplay = () => {
         if (page === 0 && !loading)
-            return <Part1 setTeamSize={setTeamSize} setTeamName={setTeamName}/>
+            return <Part1 setTeamSize={setTeamSize} setTeamName={setTeamName} />
         else if (page === 1 && !loading)
-            return <Part2 setTeamDetails={setTeamDetails} teamDetails={teamDetails}/>
+            return <Part2 setTeamDetails={setTeamDetails} teamDetails={teamDetails} />
         else if (page === 2 && !loading)
-            return <Part3 setDomain={setDomain} setAbstract={setAbstract}/>
+            return <Part3 setDomain={setDomain} setAbstract={setAbstract} />
         else if (loading)
-            return <div>Loading</div>
+            return <Loader/>
     }
 
-    const handleNext = async() => {
-        if(page===0){
-            if(teamName===""){
+    const handleNext = async () => {
+        if (page === 0) {
+            if (teamName === "") {
                 alert("Please fill the team name");
-            }else if(teamSize===0){
+            } else if (teamSize === 0) {
                 alert("Please select the team size");
-            }else{
+            } else {
                 setPage((currPage) => currPage + 1);
             }
-        }else if(page===1){
-            await setTeamDetails(teamDetails.filter(item => item ));
-            if(teamDetails.length < teamSize){
+        } else if (page === 1) {
+            await setTeamDetails(teamDetails.filter(item => item));
+            if (teamDetails.length < teamSize) {
                 alert("Please fill all the member details");
                 return;
             }
-            for(let i=0; i<teamSize;i++){
+            for (let i = 0; i < teamSize; i++) {
                 let vals = Object.values(teamDetails[i]);
-                if(vals.length<4){
-                    alert("please fill all the details of " + (i===0?"leader":"member "+ (i)))
+                if (vals.length < 4) {
+                    alert("please fill all the details of " + (i === 0 ? "leader" : "member " + (i)))
                     return;
                 }
-                for(let j=0; j<vals.length; j++)
-                    if(vals[j]===""){
+                for (let j = 0; j < vals.length; j++)
+                    if (vals[j] === "") {
                         alert("Please fill all the member details");
                         return;
-                    }else if(i===0){
-                        if(teamDetails[0].leadMobile.length!==10 || !(/^[6789]\d{9}$/.test(teamDetails[0].leadMobile))){
+                    } else if (i === 0) {
+                        if (teamDetails[0].leadMobile.length !== 10 || !(/^[6789]\d{9}$/.test(teamDetails[0].leadMobile))) {
                             alert("Team lead mobile number not valid")
                             return;
                         }
-                        if(!(/^\S+@\S+\.\S+$/).test(teamDetails[0].leadEmail)){
+                        if (!(/^\S+@\S+\.\S+$/).test(teamDetails[0].leadEmail)) {
                             alert("Team lead email id not valid")
                             return;
                         }
-                    }else{
-                        const mobile = "m"+i+"Mobile";
-                        const email = "m"+i+"Email";
-                        if(teamDetails[i][mobile].length!==10 || !(/^[6789]\d{9}$/.test(teamDetails[i][mobile]))){
+                    } else {
+                        const mobile = "m" + i + "Mobile";
+                        const email = "m" + i + "Email";
+                        if (teamDetails[i][mobile].length !== 10 || !(/^[6789]\d{9}$/.test(teamDetails[i][mobile]))) {
                             alert("Team member " + i + " mobile number not valid")
                             return;
                         }
-                        if(!(/^\S+@\S+\.\S+$/).test(teamDetails[i][email])){
+                        if (!(/^\S+@\S+\.\S+$/).test(teamDetails[i][email])) {
                             alert("Team member " + i + " email id not valid")
                             return;
                         }
@@ -108,11 +108,11 @@ export default function Registration() {
             }
             setPage((currPage) => currPage + 1);
         }
-        else if(page===titles.length-1){
-            if(domain===""){
+        else if (page === titles.length - 1) {
+            if (domain === "") {
                 alert("please select a domain");
             }
-            else{
+            else {
                 handleSubmit();
             }
         }
@@ -123,6 +123,16 @@ export default function Registration() {
         <userContext.Provider value={{ teamSize: teamSize }}>
             <div className="registration">
                 <div className="registration-main-container">
+                    <div className="home-navabar-wrapper">
+                        <div className="home-navabar-container">
+                            <ul className='home-unodered-list'>
+                                <li><p>Home</p></li>
+                                <li><p>Registration</p></li>
+                                <li><p>About Us</p></li>
+                                <li><p>Contact Us</p></li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className="heading-container">
                         <h1 className="heading-text">REGISTER</h1>
                     </div>
@@ -145,12 +155,13 @@ export default function Registration() {
                                 >Prev</button>
                                 {
                                     !(page === titles.length - 1) && <button
-                                    disabled={page === titles.length - 1}
-                                    onClick={handleNext}
-                                >Next</button>}
+                                        disabled={page === titles.length - 1}
+                                        onClick={handleNext}
+                                    >Next</button>}
                                 {
                                     (page === titles.length - 1) && <button
                                         onClick={handleNext}
+                                        disabled={loading}
                                     >Submit</button>
                                 }
                             </div>
--- src/components/forms/Part-1.jsx
+++ src/components/forms/Part-1.jsx
@@ -7,13 +7,14 @@ export default function Part(props) {
 
   useEffect(()=>{
     props.setTeamSize(0);
+    props.setTeamName("");
   },[]);
 
   return (
     <Form className='part-1-main-container'>
       <Form.Group className="drop-down-container">
           <Form.Label htmlFor="teamname" className='label'>Team Name :</Form.Label>
-          <Form.Control name='teamname' size="lg" className='input-box' type="text" onChange={(e) => {props.setTeamName(e.target.value)}}/>
+          <Form.Control name='teamname' size="lg" id="size" className='input-box' type="text" onChange={(e) => {props.setTeamName(e.target.value)}}/>
         </Form.Group>
       <Form.Group className="drop-down-container">
         <Form.Label htmlFor="size" className='label'>Team Size :</Form.Label>
@@ -26,7 +27,7 @@ export default function Part(props) {
       </Form.Group>
       <Form.Group className="abstract-container">
         <p className='abstract-text'> Sample Abstract :</p>
-        <a href="http://" target="_blank" rel="noopener noreferrer">Abstract</a>
+        <a href="https://docs.google.com/document/d/1rbljprsyUYE1Y8nNQO5Kh3DFrjILUE3t/edit?usp=sharing&ouid=101729110658745518569&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Abstract</a>
       </Form.Group>
     </Form>
   )
