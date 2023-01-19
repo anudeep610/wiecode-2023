a89a057a6578c2c924150cd46815b3b620b857c3
RanganadhY <ranganadhyadlapalli@outlook.com>
2023-01-19 15:49:46 +0530

    severing the f and b

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance=axios.create({
-    baseURL:"http://ec2-54-238-226-33.ap-northeast-1.compute.amazonaws.com:4000",
+    // baseURL:"http://ec2-54-238-226-33.ap-northeast-1.compute.amazonaws.com:4000",
     // baseURL:"http://localhost:4000",
 });
 
